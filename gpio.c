d87132a8f339e9b92aface3f2ba4a782163dee7e
takaito <taka.ito.yk@gmail.com>
2015-01-08 11:42:56 +0900

    gpio周りを書いた(とりあえず点灯だけ)

--- gpio.c
+++ gpio.c
@@ -7,22 +7,55 @@
 
 static led_t led_status;
 
+int led_init(void)
+{
+    led_status.color = RED;
+    led_status.duty = 0;
+    led_status.mode = OFF;
+    return 0;
+}
+
 int led_set_color(int new_color)
 {
+    if (new_color >= LED_COLOR_NUM || new_color < 0) return 1;
+
+    led_status.color = new_color;
     return 0;
 }
 
 int led_set_mode(int new_mode)
 {
+    if (new_mode != ON || new_mode != OFF) return 1;
+
+    led_status.mode = new_mode;
     return 0;
 }
 
 int led_set_duty(int new_duty)
 {
+    led_status.duty = new_duty;
     return 0;
 }
 
 int led_status_update(void)
 {
+    if (led_status.mode) {
+	switch(led_status.color) {
+	case RED:
+	     /* digitalWrite(LED_R_PIN, 1); */
+	     break;
+	case BLUE:
+	     /* digitalWrite(LED_R_PIN, 1); */
+	     break;
+	case GREEN:
+	     /* digitalWrite(LED_R_PIN, 1); */
+	     break;
+	default:
+	     /* digitalWrite(LED_R_PIN, 0); */
+	     /* digitalWrite(LED_G_PIN, 0); */
+	     /* digitalWrite(LED_B_PIN, 0); */
+	     break;
+	}
+    }
     return 0;
 }
--- gpio.h
+++ gpio.h
@@ -1,12 +1,26 @@
 #ifndef MY_GPIO_H
 #define MY_GPIO_H
 
+#define LED_R_PIN 22
+#define LED_B_PIN 17
+#define LED_G_PIN 27 
+
+#define ON 1
+#define OFF 0
+
+#define RED 0
+#define BLUE 1
+#define GREEN 2
+#define LED_COLOR_NUM 3
+
 typedef struct _led
 {
     int color;
     int duty;
     int mode;
 }led_t;
+
+int led_init(void);
 
 int led_set_color(int new_color);
 
