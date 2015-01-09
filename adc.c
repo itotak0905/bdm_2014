ef13b22e0cf8aca3fe587732f06fc10abb4d8752
takaito <taka.ito.yk@gmail.com>
2015-01-09 13:37:50 +0900

    adcのコメントを外した

--- adc.c
+++ adc.c
@@ -28,22 +28,22 @@ int adc_get_value(int ch)
 {
     //mcp3204用
     *buff = 0b00000110; //送信用データをバッファにセット(先頭5bitはダミー)
-    /* digitalWrite(SPI_SS_PORT, 0); //SS信号をLOW出力にして通信開始 */
-    /* wiringPiSPIDataRW(ch, buff, 1); //データ送受信 */
+    digitalWrite(SPI_SS_PORT, 0); //SS信号をLOW出力にして通信開始
+    wiringPiSPIDataRW(ch, buff, 1); //データ送受信
 
     *buff = 0b00000000; //送信用データをバッファにセット(後方6bitはダミー)
-    /* wiringPiSPIDataRW(ch, buff, 1); //データ送受信 */
+    wiringPiSPIDataRW(ch, buff, 1); //データ送受信
     adc_value = ((*buff&0xF) << 8);
 
     *buff = 0; //送信用データをバッファにセット(ダミー)
-    /* wiringPiSPIDataRW(ch, buff, 1); //データ送受信 */
-    /* digitalWrite(SPI_SS_PORT, 1); //SS信号をHIGH出力にして通信終了 */
+    wiringPiSPIDataRW(ch, buff, 1); //データ送受信
+    digitalWrite(SPI_SS_PORT, 1); //SS信号をHIGH出力にして通信終了
     adc_value += *buff;   
  
     printf ("ch  :\traw,\tvoltege\n");
     printf ("ch0 :\t%4d,\t%3.2lf\n", adc_value, adc_value*V_REF/4096);
 
-    return 0;
+    return adc_value;
 }
 
 int free_buff(void)
