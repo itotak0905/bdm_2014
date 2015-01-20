749e5f0f9747e5f4adf13af7e8d43d94b40ab5b9
takaito <taka.ito.yk@gmail.com>
2015-01-20 16:49:32 +0900

    ループん中もとりあえず書いた

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(void)
     /* init周りゴニョゴニョ */
     all_init();
 
-    int sock_brain, sock_heart;
+    int sock_brain, sock_heart, addrlen;
     struct sockaddr_in addr_brain, addr_heart;
     char buf[BUF_SIZE];
 
@@ -25,9 +25,43 @@ int main(void)
 
     printf("#test output\n");
     while(1){
-	
+
+	//心拍の処理
+	addrlen = sizeof(sock_heart);
+	n = recvfrom(sock_heart, buf, sizeof(buf), 0, (struct sockaddr *)&addr_heart, &addrlen);
+	if (n < 1) {
+		if (errno == EAGAIN) {
+			/* まだ来ない。*/
+			printf("MADA KONAI heart\n");
+		} else {
+			perror("recv");
+			break;
+		}
+	} else {
+		printf("received data heart\n");
+		printf("%s\n", buf);
+		break;
+	}
+
+	//脳波の処理
+	addrlen = sizeof(sock_brain);
+	n = recvfrom(sock_brain, buf, sizeof(buf), 0, (struct sockaddr *)&addr_brain, &addrlen);
+	if (n < 1) {
+		if (errno == EAGAIN) {
+			/* まだ来ない。*/
+			printf("MADA KONAI brain\n");
+		} else {
+			perror("recv");
+			break;
+		}
+	} else {
+		printf("received data brain\n");
+		printf("%s\n", buf);
+		break;
+	}
+
 	all_led_status_update();
-	usleep(1*1000);
+	usleep(1*1000); //1ms
     }
     
     free_buff();
@@ -55,9 +89,12 @@ int main(void)
     int fd = serial_open();
 
     while(1){
+
 	listen_once(fd);
 	meditation = get_meditation();
 	attention = get_attention();
+
+	sendto(sock, "brain\n", 6, 0, (struct sockaddr *)&addr, sizeof(addr));
     }
     serial_close(fd);    
     free_buff();
@@ -85,6 +122,7 @@ int main(void)
     int i,value[SPAN],sec=0,total=0;
     for (i=0;i<SPAN;i++) value[i]=0;
     while(1){
+	
 	total -= value[sec%SPAN];
 	value[sec%SPAN] = adc_get_value(0);
 	total += value[sec%SPAN];
@@ -94,6 +132,9 @@ int main(void)
 	} else {
 	    printf("\n");
 	}
+
+	sendto(sock, "heart\n", 6, 0, (struct sockaddr *)&addr, sizeof(addr));
+
 	sec++;
 	usleep(1*1000); /* 1ms */
     }
